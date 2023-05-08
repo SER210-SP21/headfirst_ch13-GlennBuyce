035cd02f58049a77c671b96859b97919911272b8
GlennBuyce <ghbuyce@quinnipiac.edu>
2023-05-07 23:18:22 -0400

    Chapter 11 Demo

---
+++ app/src/main/java/com/example/guessinggame/resultFragment.kt
@@ -0,0 +1,41 @@
+package com.example.guessinggame
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.example.guessinggame.databinding.FragmentResultBinding
+
+class resultFragment : Fragment() {
+    private var _binding: FragmentResultBinding? = null
+    private val binding get() = _binding!!
+    lateinit var viewModel: ResultViewModel
+    lateinit var viewModelFactory: ResultViewModelFactory
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentResultBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        val result = resultFragmentArgs.fromBundle(requireArguments()).result
+        viewModelFactory = ResultViewModelFactory(result)
+        viewModel = ViewModelProvider(this, viewModelFactory)
+            .get(ResultViewModel::class.java)
+        binding.wonLost.text = viewModel.result
+
+        binding.newGameButton.setOnClickListener {
+            view.findNavController()
+                .navigate(R.id.action_resultFragment_to_gameFragment)
+        }
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
